8a707c0c04d8c4c41ac0c1d85304f333fcba60ee
yeojang <onyshup@gmail.com>
2019-04-25 21:41:54 +0900

    about table function changed

--- exchange.c
+++ exchange.c
@@ -24,13 +24,13 @@ void main(int argc, char* argv[])
 
 void update_d_table(char ** remote_near_node,int i){
 	/*
-	  To do : 
-	  1. 전 행의 최솟 값이었던 애는 INF로 바꾸기
-	  2. 나머지는 전행의 값들 복사해오기
-	  3. 계산을 통해 비교해라 
-	  4. 네모를 구해라(네모는 INF+1 직전의 값)
-	  5. 네모를 구했으면 어제 말한 규칙으로 새로운 전임자 어레이 만들기
-	  	어제 말한 규칙이란 네모와 같은 숫자가 나온 최초의 row 값 
+	   To do : 
+	   1. 전 행의 최솟 값이었던 애는 INF로 바꾸기
+	   2. 나머지는 전행의 값들 복사해오기
+	   3. 계산을 통해 비교해라 
+	   4. 네모를 구해라(네모는 INF+1 직전의 값)
+	   5. 네모를 구했으면 어제 말한 규칙으로 새로운 전임자 어레이 만들기
+	   어제 말한 규칙이란 네모와 같은 숫자가 나온 최초의 row 값 
 	 */
 	for(int k=1;k<11;k+=2){
 		d_table[i+1][atoi(&remote_near_node[k][14])]=atoi(remote_near_node[k+1]);
@@ -46,8 +46,8 @@ void dijkstra(){
 		d_table[i+1][0]=tmp;
 		sprintf(local_addr,"%s%d",prefix_addr,find_min_w(i));//make full address
 		remote_near_node=get_nearnode_info(local_addr);
-		update_d_table(remote_near_node);
-		
+		update_d_table(remote_near_node, i);
+
 	}
 }
 
@@ -55,12 +55,12 @@ int find_min_w(int row){
 	int min= INF;
 	int mark;
 	int tmp,col;
-	
+
 	for(int i=1;i<7;i++){
 		if(d_table[row][0]==d_table[0][i]){
 			tmp=d_table[row][i];
 			col=i;
-			d_atble[row][i]=INF;
+			d_table[row][i]=INF;
 			break;
 		}
 	}
@@ -74,6 +74,41 @@ int find_min_w(int row){
 
 	d_table[row][col]=tmp;
 	return mark;
+}
+void print_d_table(){
+	for(int k=0;k<7;k++){
+		for(int j=0; j<7;j++)
+			printf("%9d\t",d_table[k][j]);
+		printf("\n");
+	}
+}
+
+void init_table(){
+	for(int k=0;k<7;k++){
+		for(int j=0; j<7;j++){
+			d_table[k][j]=INF;
+		}
+	}
+	for(int k =1; k< 7; k++){
+		d_table[0][k]=210 + k;
+	}
+	d_table[0][0] = 0;
+	d_table[0][6] = 144;
+}
+
+void init_d_table(char machine){
+	init_table();
+	int machine_index=atoi(&machine);
+	int col_num;
+	for(int k =1; k< near_node_sz; k+=2){
+		d_table[1][atoi(&near_node[k][14])]=atoi(near_node[k+1]);
+	}
+	if(machine_index == 6)
+		col_num = 144;
+	else
+		col_num = 210 + machine_index;
+
+	d_table[1][0] = col_num;
 }
 
 char **  get_nearnode_info(char* destip){
@@ -92,7 +127,6 @@ char **  get_nearnode_info(char* destip){
 	ret=-1;
 	while(ret==-1)
 	{
-		ret=connect(fd_sock, (struct sockaddr *)&addr, sizeof(addr));
 	}
 	printf("connection established\n");
 	send(fd_sock,"request near_node",sizeof("request near_node"),0);
@@ -107,31 +141,6 @@ char **  get_nearnode_info(char* destip){
 	}
 
 	return remote_near_node;
-}
-
-void print_d_table(){
-	for(int k=1;k<7;k++){
-		for(int j=0; j<7;j++)
-			printf("%9d\t",d_table[k][j]);
-		printf("\n");
-	}
-}
-
-void init_table(){
-	for(int k=0;k<7;k++){
-		for(int j=0; j<7;j++){
-			d_table[k][j]=INF;
-		}
-	}
-}
-
-void init_d_table(char machine){
-	//TODO : add addrss at row 0 and col 0
-	init_table();
-	int machine_index=atoi(&machine);
-	for(int k =1; k< near_node_sz; k+=2){
-		d_table[1][atoi(&near_node[k][14])]=atoi(near_node[k+1]);
-	}
 }
 
 void near_node_info(char* argv){
